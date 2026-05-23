b3bf27611de3066046ad8577b5bae9183def475f
Luisorozco42 <lorozcolazo@gmail.com>
2026-05-23 16:52:34 -0600

    entity created

---
+++ src/main/java/gm/contactos/modelo/Contacto.java
@@ -0,0 +1,26 @@
+package gm.contactos.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class Contacto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idContacto;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+
+}
